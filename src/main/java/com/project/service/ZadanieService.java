21a78fea6daceabeee61f7efbd78a98b3af30756
Michał Kardasz <michalkardasz03@gmail.com>
2025-06-02 15:37:51 +0200

    dodano zadanierepository

--- src/main/java/com/project/service/ZadanieService.java
+++ src/main/java/com/project/service/ZadanieService.java
@@ -1,5 +1,22 @@
 package com.project.service;
 
+import com.project.model.Zadanie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface ZadanieService {
 
+    Zadanie setZadanie(Zadanie zadanie);
+
+    Optional<Zadanie> getZadanieById(Integer id);
+
+    void deleteZadanieById(Integer id);
+
+    Zadanie updateZadanie(Integer id, Zadanie updatedZadanie);
+
+    Page<Zadanie> getAllZadania(Pageable pageable);
+
+    Page<Zadanie> getZadaniaByProjektId(Integer projektId, Pageable pageable);
 }
